2e9636dc133cef6b0fc89629e07dbae60eeaf093
lookup-1922 <125456965+lookup-1922@users.noreply.github.com>
2023-10-02 20:08:17 +0900

    簡易的な素数判定を追加

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,21 @@ use std::io;
 fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).ok();
-    let number:i128 = input.trim().parse().ok().unwrap();
-    println!("{}",number);
+    let number_p: u128 = input.trim().parse().ok().unwrap();
+    let result = is_prime(&number_p);
+    println!("{}", result);
 }
+
+fn is_prime(number: &u128) -> bool {
+    if number <= &1 {
+        return false;
+    }
+    for i in 2..*number {
+        if number % i == 0 {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn is_pefect_number(number: i64) {}
